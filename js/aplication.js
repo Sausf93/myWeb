2d128e1079e8e837c93b9073a83b3a0600587951
Sausf93 <Saulodlsf@gmail.com>
2018-03-12 19:33:36 +0100

    Add files via upload

---
+++ js/aplication.js
@@ -0,0 +1,15 @@
+function comecocos(){
+    window.location.href="https://sausf93.github.io/Comecocos/";
+}
+
+function bestbuy(){
+    window.location.href="https://emimontesdeoca.github.io/BestShopping/";
+}
+
+function Marvel(){
+    window.location.href="https://sausf93.github.io/Marvel/";
+}
+
+function Cine(){
+    window.location.href="https://sausf93.github.io/Pagina-de-cine-final/";
+}
